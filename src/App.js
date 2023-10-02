8c577f4276bb1bc5b15483d70cdb3f1737dda40a
khaled-hoshan <111223587+khaled-hoshan@users.noreply.github.com>
2023-10-02 23:39:34 +0300

    adding the navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import {
+	NavBar,
+	Feed,
+	VideoDetail,
+	ChannelDetail,
+	SearchFeed,
+} from './components';
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => (
+	<BrowserRouter>
+		<Box sx={{ backgroundColor: '#000' }}>
+			<NavBar />
+			<Routes>
+				<Route path='/' exact element={<Feed />} />
+				<Route path='/video/:id' exact element={<VideoDetail />} />
+				<Route path='/channel/:id' exact element={<ChannelDetail />} />
+				<Route path='/search/:searchTerm' exact element={<SearchFeed />} />
+			</Routes>
+		</Box>
+	</BrowserRouter>
+);
 
 export default App;
